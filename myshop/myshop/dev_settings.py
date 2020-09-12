999a132d63f7d75b58d6ba2cccd1561999970a22
kostya <skmwork@yandex.ru>
2020-09-12 11:42:25 +0300

    dev_settings

---
+++ myshop/myshop/dev_settings.py
@@ -0,0 +1,7 @@
+from .settings import *
+
+DEBUG = True
+#MIDDLEWARE.remove('django.middleware.cache.UpdateCacheMiddleware')
+#MIDDLEWARE.remove('django.middleware.cache.FetchFromCacheMiddleware')
+#del CACHES
+ROOT_URLCONF = 'myshop.dev_urls'
---
+++ myshop/myshop/dev_urls.py
@@ -0,0 +1,3 @@
+from .urls import urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
