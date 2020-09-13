9784d7ea821c8690f20dace6530c0310d74b419d
kostya <skmwork@yandex.ru>
2020-09-13 23:31:07 +0300

    pagination

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage, Page
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
@@ -6,10 +7,18 @@ from cart.forms import CartAddProductForm
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
+    products_list = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
+        products_list = products_list.filter(category=category)
+    paginator = Paginator(products_list, 1)  # 3 posts in each page
+    page = request.GET.get('page', 3)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
     return render(request,
                   'shop/product/list.html',
                   {'category': category,
