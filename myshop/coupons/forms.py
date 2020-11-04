cf8540414acb599228d9a4f6ab93cf184e7032e6
kostya <skmwork@yandex.ru>
2020-11-05 00:20:03 +0300

    html cart

--- myshop/coupons/forms.py
+++ myshop/coupons/forms.py
@@ -4,5 +4,5 @@ from django.utils.translation import gettext_lazy as _
 
 class CouponApplyForm(forms.Form):
     code = forms.CharField(label=False, widget=forms.TextInput(attrs={'placeholder': _('Coupon'),
-                                                                      'class': 'textinput textInput form-control'}))
+                                                                      'class': 'form-control'}))
 
